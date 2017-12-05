67e6c8f2e74b1b1ecd042796aefdf6e0c2c399f8
AndrewWargo <awargo@stuy.edu>
2017-12-04 21:05:41 -0500

    First Commit

---
+++ RPGv2/Character.java
@@ -0,0 +1,30 @@
+
+public class Character {
+
+    protected int HP;
+    protected int strength;
+    protected int defense;
+    protected double attackRating;
+	
+    public boolean isAlive(){
+    	return HP > 0;
+    }
+
+    public int getDefense(){
+    	return defense;
+    }
+
+    public void lowerHP(int damage){
+    	HP = HP - damage;
+    	if (!isAlive())
+    	    System.out.println("You died");
+    }
+
+    public int attack(Character target){
+    	int damage = (int)((double)strength * attackRating) - target.getDefense();
+	if (damage < 0)
+	    damage = 0;
+    	target.lowerHP(damage);
+    	return damage;
+    }
+}
---
+++ RPGv2/Monster.java
@@ -0,0 +1,11 @@
+
+public class Monster extends Character {
+	
+    public Monster(){
+	HP = 150;
+	strength = (int)(Math.random() * 45) + 20;
+	defense = 20;
+	attackRating = 1;
+    }
+
+}
